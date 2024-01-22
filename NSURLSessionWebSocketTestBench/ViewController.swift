fbb3fef17b4d2111be49365a1999f9c14d5b1cb5
Evan O'Connor <evanoc3@gmail.com>
2024-01-22 19:04:12 +0000

    Initial Commit

---
+++ NSURLSessionWebSocketTestBench/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NSURLSessionWebSocketTestBench
+//
+//  Created by Evan O'Connor on 22/01/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
